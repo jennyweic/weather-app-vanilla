9a980a968b020652b90dad0764c1ed38f2651df2
jennyweic <weicchan@gmail.com>
2023-11-14 10:30:42 +0000

    Updated main weather information

--- index.js
+++ index.js
@@ -24,6 +24,34 @@ formElement.addEventListener("click", search);
 //4 function to recieve response
 function displayCityWeather(response){
  console.log(response);
+
+ let currentTemperature = response.data.temperature.current;
+ console.log("Current Temperature is", currentTemperature);
+ let currentTempElement = document.querySelector("#temperature");
+ currentTempElement.innerHTML = `${Math.round(currentTemperature)}`;
+
+//e.g. broken clouds
+let currentDescription = response.data.condition.description;
+console.log(currentDescription);
+let currentDescriptionElement = document.querySelector("#decription");
+currentDescriptionElement.innerHTML = `${currentDescription}`;
+
+let currentFeel = response.data.temperature.feels_like;
+console.log(currentFeel);
+let currentFeelElement = document.querySelector("#feels-like-degree");
+currentFeelElement.innerHTML =`${Math.round(currentFeel)}`;
+
+let currentWind = response.data.wind.speed;
+console.log(currentWind);
+let currentWindElement = document.querySelector("#wind-speed");
+currentWindElement.innerHTML = `${Math.round(currentWind)}`;
+
+let currentHumidity = response.data.temperature.humidity;
+console.log(currentHumidity);
+let currentHumidityElement = document.querySelector("#humidity");
+currentHumidityElement.innerHTML = currentHumidity;
+
+
 }
 
 
