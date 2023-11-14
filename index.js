f3e58bb70656093bb8f20c7a452976af26eeb776
jennyweic <weicchan@gmail.com>
2023-11-14 10:02:05 +0000

    Logged the weather response data from the api

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ function search (event) {
     
     let searchInput = document.querySelector("#formInput");
     console.log(searchInput.value);
+    let cityElement = document.querySelector("h1");
+    cityElement.innerHTML =`${searchInput.value}`;
+    const city = searchInput.value;
 
-    let h1 = document.querySelector("h1");
-    h1.innerHTML =`${searchInput.value}`;
+
+
+//3 add the weather API axios cdn and check console
+let apiKey = "698445003bc2a9cbfcb050ae4t74oc8b";
+let apiURL =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+axios.get(apiURL).then(displayCityWeather);
 
 }
 // 1)search engine behaviour for cities by creating a form 
@@ -14,10 +21,16 @@ let formElement = document.querySelector("#search-function");
 formElement.addEventListener("click", search);
 
 
+//4 function to recieve response
+function displayCityWeather(response){
+ console.log(response);
+}
 
 
 
 
-// add the weather API axios and check console
+
+
+
 // make sure the date is updated aswell according to city information
 // display the weather icon
