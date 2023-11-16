77cead793586b09660902948784505e0d3413006
jennyweic <weicchan@gmail.com>
2023-11-16 09:06:35 +0000

    Update, seperate date and time to resolve js issue

--- index.js
+++ index.js
@@ -8,13 +8,10 @@ function search (event) {
     cityElement.innerHTML =`${searchInput.value}`;
     const city = searchInput.value;
 
-
-
 //3 add the weather API axios cdn and check console
 let apiKey = "698445003bc2a9cbfcb050ae4t74oc8b";
 let apiURL =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 axios.get(apiURL).then(displayCityWeather);
-
 }
 // 1)search engine behaviour for cities by creating a form 
 let formElement = document.querySelector("#search-function");
@@ -50,58 +47,46 @@ let currentHumidity = response.data.temperature.humidity;
 console.log(currentHumidity);
 let currentHumidityElement = document.querySelector("#humidity");
 currentHumidityElement.innerHTML = currentHumidity;
+}
 
-let date = new Date(response.data.time*1000)
+
+//6 function to format date
+    let now = new Date();
+    console.log(now);
+
+function formatDate(date){
+    let date = now.getDate() ;
+    let days = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"];
+    let day = days[now.getDay()];
+    let months =["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    let month = months[date.getMonth()];
+    let year = date.getFullYear();
+
+    let dayElement = document.querySelector("#day");
+    dayElement.innerHTML =`${days(date.getDay())}`
+
+    let monthElement = document.querySelector("month");
+    monthElement.innerHTML =`${months(date.getMonth())}`;
+
+    let dateElement = document.querySelector("date");
+    dateElement.innerHTML =`${date.getDate()}`;
+
+    let yearElement = document.querySelector("year");
+    yearElement.innerHTML =`${date.getFullYear()}`;
+
+//TIME UPDATE
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time*1000);
+    timeElement.innerHTML = `${date.gethours()}:${date.getMinutes()}`;
+
+    let hours =date.gethours();
+    let minutes = date.getMinutes();
+
+    let newTimeElement = `${hours()}:${minutes()}`; 
+    let newDayElement =`${day}, ${month} ${date} ${year}`;
 }
 
 
 //5 make sure the date is updated aswell according to city information
-
-
-let timeElement = document.querySelector("#time");
-timeElement.innerHTML = `${hours()}:${minutes()}`;
-
-let dayElement = document.querySelector("#day");
-dayElement.innerHTML =`${days(date.getDay())}`
-
-let monthElement = document.querySelector("month");
-monthElement.innerHTML =`${months(date.getMonth())}`;
-
-let dateElement = document.querySelector("date");
-dateElement.innerHTML =`${date.getDate()}`;
-
-let yearElement = document.querySelector("year");
-yearElement.innerHTML =`${date.getFullYear()}`;
-
-
-//6 function to format date
-function formatDate(date){
-
-//get hour
-let hours =date.gethours();
-//get minutes
-let minutes = date.getMinutes();
-
-//get date
-let date = date.getDate() ;
-//get day of the week
-let days = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"];
-let day = days(date.getDay());
-//get month
-let months =["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
-let month = months(date.getMonth());
-//get year
-let year = date.getFullYear();
-
-}
-
-
-
-
-
-
-
-
-
 // make sure the date is updated aswell according to city information
 // display the weather icon
